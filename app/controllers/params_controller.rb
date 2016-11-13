673962786ec26c52a532e32d9caf1d4cd19c04a9
Danish Mughal <mughal.danish@gmail.com>
2016-11-13 09:23:32 -0800

    Added forms params example

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -1,14 +1,38 @@
 class ParamsController < ApplicationController
 
   def query_params
+    # http://localhost:3000/params_test?message=testing&second_message=hello
     # ?Key=value
-    # ?message=whatever
-    # &second_message=whatever
+    # ?message=whatever&second_message=whatever
     @message = params[:message]
     @second_message = params[:second_message]
-
   end
 
+  def guess_a_number
+    winning_number = 32
+    chosen_number = params[:number].to_i
+
+    if chosen_number > winning_number
+      @message = "Too high. Guess lower."
+    elsif chosen_number < winning_number
+      @message = "Too low. Guess higher."
+    else
+      @message = "You got the number!"
+    end
+  end
+
+  def url_params
+    # localhost:3000/url_parameter/hello
+    @message = params[:message]
+    @second_message = params[:second_message]
+  end
+
+  def form_display
+  end
+
+  def form_result
+    @message = params[:mymessage]
+  end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,15 @@ Rails.application.routes.draw do
 
   get "/params_test", to: "params#query_params"
 
+  get "/game", to: "params#guess_a_number"
+
+  # localhost:3000/url_parameter/hello
+  get "/url_parameter/:message/:second_message", to: "params#url_params"
+
+  # Routes for form
+  get "/form_display", to: "params#form_display"
+  post "/form_result", to: "params#form_result"
+
+
 
 end
